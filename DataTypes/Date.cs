5020f213ecc80579d20cac94cc8f4c1ae68c332b
paul_calderon <paul.calderon@neu.com.ec>
2021-02-26 18:57:02 +0000

    Revert "Correction for issue #54"
    This reverts commit 0fc36c167d10e8fea398e27034285f870d7052c0.

--- DataTypes/Date.cs
+++ DataTypes/Date.cs
@@ -36,7 +36,7 @@ namespace DotNetNuke.Modules.UserDefinedTable.DataTypes
                 }
                 ctlDate.ID = CleanID(string.Format("{0}_date", FieldTitle));
                 ctlDate.CssClass = "fnl-datepicker";
-                if (Required) ctlDate.CssClass += " dnnFormRequired";
+                if (Required) ctlDate.CssClass += "dnnFormRequired";
 
                 Controls.Add(ctlDate);
                 CtlValueBox = ctlDate;
